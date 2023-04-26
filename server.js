3adc84fdeed33292c256850c910baa4f038fe99b
adamcsanyi16 <csanyiadam8@gmail.com>
2023-04-26 14:02:43 +0200

    ejs beállítása

--- server.js
+++ server.js
@@ -4,14 +4,16 @@ const app = express();
 
 const PORT = process.env.PORT || 3500;
 
+app.set('view engine', 'ejs');
+
 app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`)
+    console.log(`http://localhost:${PORT}`);
 });
 
 app.get('/', (req,res) => {
     try {
-        res.status(200).json({msg: 'Sikeres elindítás!'})
+        res.status(200).render("index");
     } catch (error) {
-        res.status(500).json({msg: 'Valami elromlott!'})
+        res.status(500).json({msg: 'Valami elromlott!'});
     }
 } )
