c8a4c77b3b59561afe010d6bd076839acbd427e1
adamcsanyi16 <csanyiadam8@gmail.com>
2023-04-26 14:20:12 +0200

    dbconnect js

--- middlewares/dbConnect.js
+++ middlewares/dbConnect.js
@@ -1,1 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+const dbConnect = () => {
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("Sikeres csatlakozás"))
+    .catch((error) => console.log("Valami hiba történt" + error.message));
+
+  mongoose.connection.on("open", () => console.log("Adatbázis megnyitva!"));
+  mongoose.connection.on("close", () => console.log("Adatbázis bezárva!"));
+};
+
+module.exports = dbConnect;
